1ad3820fee913df8b255ecbe75f2f53ead14027f
risto <risto.kowaczewski@gmail.com>
2021-11-25 21:38:27 +0100

    Make name required.

--- tracardi_rabbitmq_publisher/model/configuration.py
+++ tracardi_rabbitmq_publisher/model/configuration.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from tracardi.domain.entity import Entity
 
 
@@ -12,6 +12,12 @@ class QueueConfig(BaseModel):
     compression: Optional[str] = None
     auto_declare: bool = True
     serializer: str = 'json'
+
+    @validator("name")
+    def name_not_empty(cls, value):
+        if value and len(value) == 0:
+            raise ValueError("Name can not be empty")
+        return value
 
 
 class PluginConfiguration(BaseModel):
